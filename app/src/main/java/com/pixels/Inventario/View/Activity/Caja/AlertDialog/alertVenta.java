df92611a55f6f08af2209e5bac24b98a636063bb
raynor119 <deivicamiloflorezprieto@gmail.com>
2022-08-14 19:00:59 -0500

    Copia de Seguridad V8 Caja

--- app/src/main/java/com/pixels/Inventario/View/Activity/Caja/AlertDialog/alertVenta.java
+++ app/src/main/java/com/pixels/Inventario/View/Activity/Caja/AlertDialog/alertVenta.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -19,6 +20,8 @@ import com.pixels.Inventario.View.Activity.Gestion_Productos.AgregarProductos.Te
 import com.pixels.Inventario.ViewModel.Caja.Venta.RealizarVentaViewModel;
 import com.pixels.Inventario.ViewModel.Gestion_Productos.AgregarProductos.ConvertirModenaINT;
 
+import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class alertVenta {
@@ -63,8 +66,9 @@ public class alertVenta {
                     ConvertirModenaINT convertir=new ConvertirModenaINT();
                     int efectivo=convertir.Convertir(cantidad.getText().toString());
                     if(efectivo<total){
-                        cantidad.setError("La cantidad de efectivo tiene que ser mayor o igual al Total de la Venta");
+                        cantidad.setError("La cantidad de efectivo recibida tiene que ser mayor o igual al Total de la Venta");
                     }else{
+                        int cambio=efectivo-total;
                         RealizarVentaViewModel Venta= ViewModelProviders.of(Context).get(RealizarVentaViewModel.class);
                         Venta.reset();
                         Venta.realizarVenta(Context,efectivo);
@@ -72,7 +76,10 @@ public class alertVenta {
                             @Override
                             public void onChanged(List<VentaRealizada> ventaRealizadas) {
                                 if(ventaRealizadas.get(0).isVerificar()){
-                                    Toast.makeText(Context, "CodigoVenta: "+ventaRealizadas.get(0).getCodigoVenta(), Toast.LENGTH_LONG).show();
+                                    VentaRealizada(ventaRealizadas.get(0).getCodigoVenta(),cambio);
+                                    Context.Productos=new ArrayList<>();
+                                    Context.iniciarRecyclerView();
+                                    dialog.cancel();
                                 }else{
                                     Toast.makeText(Context, "Error al Realizar la Venta", Toast.LENGTH_LONG).show();
                                 }
@@ -84,5 +91,36 @@ public class alertVenta {
             }
         });
     }
+    public void VentaRealizada(String CodigoV,int Cambio){
+        AlertDialog.Builder builder = new AlertDialog.Builder(Context);
+        builder.setCancelable(false);
+        builder.setTitle("Venta Realizada");
+        LayoutInflater inflater= Context.getLayoutInflater();
+        View view=inflater.inflate(R.layout.alertventafinalizada, null);
+        NumberFormat formato= NumberFormat.getNumberInstance();
+        builder.setView(view);
+        TextView cambio=(TextView) view.findViewById(R.id.cambio);
+        cambio.setText("$"+formato.format(Cambio));
+        builder.setPositiveButton("Compartir La Factura", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
 
+            }
+        });
+        builder.setNegativeButton("Ver La Factura", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.setNeutralButton("Salir", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 }
