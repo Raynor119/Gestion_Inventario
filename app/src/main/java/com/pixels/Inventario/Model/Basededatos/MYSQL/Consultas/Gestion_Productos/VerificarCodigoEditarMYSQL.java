29fa99be1aba00a301c3cce49e1e916ea81435e6
Raynor119 <deivicamiloflorezprieto@gmail.com>
2022-09-06 17:17:04 -0500

    copia de seguridad v39.4 arreglo en el model verificaciones de codigos mysql

--- app/src/main/java/com/pixels/Inventario/Model/Basededatos/MYSQL/Consultas/Gestion_Productos/VerificarCodigoEditarMYSQL.java
+++ app/src/main/java/com/pixels/Inventario/Model/Basededatos/MYSQL/Consultas/Gestion_Productos/VerificarCodigoEditarMYSQL.java
@@ -83,14 +83,9 @@ public class VerificarCodigoEditarMYSQL extends Conexion implements MediadorBase
     @Override
     public void ConsultaBaseDatos() {
         boolean verificar=true;
-
         for(int i=0;i<productos.size();i++){
-            if(productos.get(i).getCodigo().equals(Codigo)){
-
-            }else {
-                if(productos.get(i).getCodigo().equals(CodigoE)){
-                    verificar=false;
-                }
+            if(!productos.get(i).getCodigo().equals(Codigo) && productos.get(i).getCodigo().equals(CodigoE)) {
+                verificar = false;
             }
         }
         ViewModel.resultado.setValue(verificar);
