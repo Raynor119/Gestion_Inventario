9f0f6f8166925616767aa8c92e8728a975c5a0c0
Raynor119 <deivicamiloflorezprieto@gmail.com>
2022-09-06 17:43:49 -0500

    copia de seguridad v40 inicigo de la funcionalidad de gestion de ventas

---
+++ app/src/main/java/com/pixels/Inventario/View/Gestion_Ventas/VerVentas.java
@@ -0,0 +1,16 @@
+package com.pixels.Inventario.View.Gestion_Ventas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pixels.Inventario.R;
+
+public class VerVentas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_ventas);
+    }
+}
