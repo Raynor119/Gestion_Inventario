eea445a6cdd9ec069265a617b8c719e7ad886ebb
Raynor119 <deivicamiloflorezprieto@gmail.com>
2022-09-11 23:49:46 -0500

    copia de seguridad v44.7 agregar fechas aparente mente no inestable

--- app/src/main/java/com/pixels/Inventario/View/Gestion_Ventas/TabLayout/Fragment/AnualesFragment.java
+++ app/src/main/java/com/pixels/Inventario/View/Gestion_Ventas/TabLayout/Fragment/AnualesFragment.java
@@ -5,11 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.pixels.Inventario.R;
+import com.pixels.Inventario.View.Gestion_Ventas.TabLayout.Calendario.AnnoPickerDialog;
+import com.pixels.Inventario.View.Gestion_Ventas.TabLayout.Calendario.MesAnnoPickerDialog;
+
+import java.util.Calendar;
 
 public class AnualesFragment extends Fragment {
+    public TextInputEditText calendarioEditText;
     public AnualesFragment(){
 
     }
@@ -20,6 +27,24 @@ public class AnualesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_anuales, container, false);
+        calendarioEditText=(TextInputEditText) rootView.findViewById(R.id.fecha);
+        calendarioEditText.setEnabled(false);
+        calendarioEditText.setText(getAnno());
+        CardView Bcalendario=(CardView) rootView.findViewById(R.id.calendario);
+        Bcalendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int anno;
+                anno=Integer.parseInt(calendarioEditText.getText().toString());
+                AnnoPickerDialog pd = new AnnoPickerDialog(AnualesFragment.this,anno);
+                pd.SelectFecha();
+            }
+        });
         return rootView;
+    }
+    public String getAnno(){
+        Calendar calendar= Calendar.getInstance();
+        int anno=calendar.get(Calendar.YEAR);
+        return ""+anno;
     }
 }
--- app/src/main/java/com/pixels/Inventario/View/Gestion_Ventas/TabLayout/Fragment/MensualesFragment.java
+++ app/src/main/java/com/pixels/Inventario/View/Gestion_Ventas/TabLayout/Fragment/MensualesFragment.java
@@ -1,13 +1,10 @@
 package com.pixels.Inventario.View.Gestion_Ventas.TabLayout.Fragment;
 
-import android.app.DatePickerDialog;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
-import android.widget.Toast;
+
 
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
