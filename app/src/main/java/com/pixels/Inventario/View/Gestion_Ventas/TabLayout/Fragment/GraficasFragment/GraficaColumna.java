9be88e14f1795d2e9f7f52d367d908d9274978e2
Raynor119 <deivicamiloflorezprieto@gmail.com>
2022-09-19 01:00:36 -0500

    copia de seguridad v49.2 Creacion de la grafica de productor mas vendidos

---
+++ app/src/main/java/com/pixels/Inventario/View/Gestion_Ventas/TabLayout/Fragment/GraficasFragment/GraficaColumna.java
@@ -0,0 +1,9 @@
+package com.pixels.Inventario.View.Gestion_Ventas.TabLayout.Fragment.GraficasFragment;
+
+import androidx.fragment.app.Fragment;
+
+public class GraficaColumna extends Fragment {
+    public GraficaColumna(){
+
+    }
+}
