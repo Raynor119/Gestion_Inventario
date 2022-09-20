7da20841fd772c5abe88721ba32a516f39b9790b
Raynor119 <deivicamiloflorezprieto@gmail.com>
2022-09-20 01:10:35 -0500

    copia de seguridad v49.6.2 agregando a las otros viewpagers

---
+++ app/src/main/java/com/pixels/Inventario/Model/Basededatos/MYSQL/Consultas/Gestion_Ventas/GraficasFragment/GColumnas/ProductosVendidosMMYSQL.java
@@ -0,0 +1,77 @@
+package com.pixels.Inventario.Model.Basededatos.MYSQL.Consultas.Gestion_Ventas.GraficasFragment.GColumnas;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.pixels.Inventario.Model.Basededatos.MYSQL.Conexion;
+import com.pixels.Inventario.Model.Basededatos.MediadorBaseDatos;
+import com.pixels.Inventario.Model.DatosE.DatosColumn;
+import com.pixels.Inventario.ViewModel.Gestion_Ventas.GraficasFragment.GColumnas.GraficaColumnDRecyclerViewModel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductosVendidosMMYSQL extends Conexion implements MediadorBaseDatos {
+    public GraficaColumnDRecyclerViewModel ViewModel;
+    public List<DatosColumn> ProductosV;
+    private boolean verificarE=false;
+    private String Consulta="";
+    public ProductosVendidosMMYSQL(Context context, GraficaColumnDRecyclerViewModel viewModel, String consulta) {
+        super(context);
+        this.ViewModel=viewModel;
+        this.Consulta=consulta;
+        execute("");
+        new android.os.Handler().postDelayed(new Runnable() {
+            public void run() {
+                if(verificarE){
+                    onCancelled();
+                }else{
+                    onCancelled("No se puede conectar a La Base Datos");
+                    verificarE=true;
+                }
+            }
+        },31000);
+    }
+    @Override
+    protected String doInBackground(String... params) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            DriverManager.setLoginTimeout(30);
+            Connection connection= DriverManager.getConnection(Url,Usuario,Contra);
+            if(verificarE){
+                return "Error en la conexion";
+            }else{
+                Statement st = connection.createStatement();
+                ResultSet rs = st.executeQuery(Consulta);
+                ProductosV=new ArrayList<>();
+                while (rs.next()) {
+                    ProductosV.add(new DatosColumn(rs.getString(1),rs.getString(2),rs.getInt(3)));
+                }
+                return "";
+            }
+        }catch (Exception e){
+            return "No se puede conectar a La Base Datos";
+        }
+    }
+    @Override
+    protected void onPostExecute(String result) {
+        verificarE=true;
+        if(result.equals("")){
+            ConsultaBaseDatos();
+        }else {
+            if(result.equals("Error en la conexion")){
+
+            }else{
+                Toast.makeText(Context, result, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    @Override
+    public void ConsultaBaseDatos() {
+        ViewModel.resultado.setValue(ProductosV);
+    }
+}
---
+++ app/src/main/java/com/pixels/Inventario/Model/Basededatos/SQLite/BaseDatos/Consultas/Gestion_Ventas/GraficasFragment/GColumnas/ProductosVendidosASQLite.java
@@ -0,0 +1,41 @@
+package com.pixels.Inventario.Model.Basededatos.SQLite.BaseDatos.Consultas.Gestion_Ventas.GraficasFragment.GColumnas;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.pixels.Inventario.Model.Basededatos.MediadorBaseDatos;
+import com.pixels.Inventario.Model.Basededatos.SQLite.BaseDatos.BaseDatosSQLite;
+import com.pixels.Inventario.Model.DatosE.DatosColumn;
+import com.pixels.Inventario.ViewModel.Gestion_Ventas.GraficasFragment.GColumnas.GraficaColumnDRecyclerViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductosVendidosASQLite extends BaseDatosSQLite implements MediadorBaseDatos {
+
+    public GraficaColumnDRecyclerViewModel ViewModel;
+    private String Consulta="";
+    public ProductosVendidosASQLite(Context context, GraficaColumnDRecyclerViewModel viewModel, String consulta) {
+        super(context);
+        this.ViewModel=viewModel;
+        this.Consulta=consulta;
+        ConsultaBaseDatos();
+    }
+    public List<DatosColumn> DatosProductos(){
+        List<DatosColumn> datos=new ArrayList<>();
+        SQLiteDatabase bd=getReadableDatabase();
+        Cursor cursor=bd.rawQuery(Consulta,null);
+        if(cursor.moveToFirst()){
+            do{
+                datos.add(new DatosColumn(cursor.getString(0),cursor.getString(1),cursor.getInt(2)));
+            }while(cursor.moveToNext());
+        }
+        return datos;
+    }
+
+    @Override
+    public void ConsultaBaseDatos() {
+        ViewModel.resultado.setValue(DatosProductos());
+    }
+}
