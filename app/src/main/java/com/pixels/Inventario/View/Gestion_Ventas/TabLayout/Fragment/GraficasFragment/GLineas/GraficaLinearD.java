50a29feace0d013f5734ba6230177e0dd514edad
Raynor119 <deivicamiloflorezprieto@gmail.com>
2022-09-20 20:48:18 -0500

    copia de seguridad v49.8.1 creacion de la grafica de lineas

--- app/src/main/java/com/pixels/Inventario/View/Gestion_Ventas/TabLayout/Fragment/GraficasFragment/GLineas/GraficaLinearD.java
+++ app/src/main/java/com/pixels/Inventario/View/Gestion_Ventas/TabLayout/Fragment/GraficasFragment/GLineas/GraficaLinearD.java
@@ -9,16 +9,30 @@ import androidx.fragment.app.Fragment;
 
 import com.github.mikephil.charting.charts.LineChart;
 
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.pixels.Inventario.Model.DatosE.TotalVentas;
 import com.pixels.Inventario.R;
 
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
 public class GraficaLinearD extends Fragment {
     public static List<TotalVentas> VentasD;
 
-    private LineChart GColumna;
+    private LineChart GLinear;
+    private ArrayList<Entry> data;
 
     public GraficaLinearD(){
 
@@ -29,11 +43,107 @@ public class GraficaLinearD extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragmentgraficalinear, container, false);
-        GColumna = (LineChart) rootView.findViewById(R.id.chart_bottom);
+        GLinear = (LineChart) rootView.findViewById(R.id.chart_bottom);
         GenerarGrafica();
         return rootView;
     }
     public void GenerarGrafica(){
+        data = new ArrayList<Entry>();
+        for(int i=0;i<VentasD.size();i++){
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            try {
+                Date date = simpleDateFormat.parse(VentasD.get(i).getFecha());
+                date.getHours();
+                data.add(new Entry(((float) (date.getHours()+(date.getMinutes()*0.0166667))),((float)VentasD.get(i).getTotalV())));
+            }catch (Exception e){
+
+            }
+        }
+        LineDataSet lineDataSet=new LineDataSet(data,"Total Vendido");
+        lineDataSet.setColors(ColorTemplate.rgb("0090FD"));
+        lineDataSet.setCircleColors(ColorTemplate.rgb("0090FD"));
+        lineDataSet.setCircleRadius(6f);
+        lineDataSet.setValueTextSize(11);
+        lineDataSet.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                NumberFormat mFormat = NumberFormat.getNumberInstance();
+                return "$ "+mFormat.format(value);
+            }
+        });
+        ArrayList<ILineDataSet> dataSets=new ArrayList<>();
+        dataSets.add(lineDataSet);
+
+        LineData Ldata=new LineData(dataSets);
+        Description description=new Description();
+        description.setText("Total Vendido");
+        GLinear.setData(Ldata);
+        GLinear.setDescription(description);
+        //Para que las lineas sean Curbas
+        List<ILineDataSet> sets = GLinear.getData().getDataSets();
+        for (ILineDataSet iSet : sets) {
+
+            LineDataSet set = (LineDataSet) iSet;
+            set.setMode(set.getMode() == LineDataSet.Mode.HORIZONTAL_BEZIER
+                    ? LineDataSet.Mode.LINEAR
+                    :  LineDataSet.Mode.HORIZONTAL_BEZIER);
+        }
+        GLinear.invalidate();
+        GLinear.setDoubleTapToZoomEnabled(false);
+
+        XAxis xAxis =GLinear.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setAxisMaximum(24);
+        xAxis.setAxisMinimum(0);
+        xAxis.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                int horas=((int) value);
+                int minutos=((int) (60 * (value - horas)));
+                int segundos=((int) (60 * ((60 * (value - horas) - minutos))));
+                String formato="";
+                if(minutos>9){
+                    if(segundos>0){
+                        if (segundos>9){
+                            formato=""+horas+":"+minutos+":"+segundos;
+                        }else{
+                            formato=""+horas+":"+minutos+":0"+segundos;
+                        }
+                    }else{
+                        formato=""+horas+":"+minutos;
+                    }
+                }else{
+                    if(segundos>0){
+                        if(segundos>9){
+                            formato=""+horas+":0"+minutos+":"+segundos;
+                        }else{
+                            formato=""+horas+":0"+minutos+":0"+segundos;
+                        }
+                    }else{
+                        formato=""+horas+":0"+minutos;
+                    }
+                }
+                return formato;
+            }
+        });
+
+        YAxis LeftAxis = GLinear.getAxisLeft();
+        LeftAxis.setDrawGridLines(false);
+        LeftAxis.setAxisMinimum(0);
+        LeftAxis.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                NumberFormat mFormat = NumberFormat.getNumberInstance();
+                return "$ "+mFormat.format(value);
+            }
+        });
+
+        YAxis rigthAxis = GLinear.getAxisRight();
+        rigthAxis.setEnabled(false);
+
+        GLinear.animateY(900);
 
     }
 
