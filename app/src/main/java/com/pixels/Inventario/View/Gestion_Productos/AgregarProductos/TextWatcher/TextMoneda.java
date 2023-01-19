708ffba72290c2f5df19b8db3f80cb59599adac7
raynor119 <deivicamiloflorezprieto@gmail.com>
2023-01-18 21:03:59 -0500

    arreglos del textwhatcher en el dinero

--- app/src/main/java/com/pixels/Inventario/View/Gestion_Productos/AgregarProductos/TextWatcher/TextMoneda.java
+++ app/src/main/java/com/pixels/Inventario/View/Gestion_Productos/AgregarProductos/TextWatcher/TextMoneda.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.pixels.Inventario.R;
 
@@ -20,6 +21,7 @@ public class TextMoneda {
     public TextWatcher moneda(final EditText editText) {
         return new TextWatcher() {
             private String current = "";
+            private String maximo="";
             private int select=-1;
             @Override
             public void afterTextChanged(Editable arg0) {
@@ -33,39 +35,49 @@ public class TextMoneda {
             }
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(!s.toString().equals(current) && s.toString().compareTo("")!=0){
-                    s=s+".00";
-                    editText.removeTextChangedListener(this);
-                    String cleanString = s.toString().replaceAll("["+Context.getResources().getString(R.string.MonedaMonto)+",.]", "").replace(" ","");
-                    double parsed = Double.parseDouble(cleanString.replaceAll("\\s","").trim());
-                    DecimalFormat decimalFormat  = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
-                    DecimalFormatSymbols symbols = decimalFormat.getDecimalFormatSymbols();
-                    symbols.setCurrencySymbol(Context.getResources().getString(R.string.MonedaMonto)+" ");
-                    decimalFormat.setDecimalFormatSymbols(symbols);
-                    decimalFormat.setMaximumFractionDigits(0);
-                    String formatted = decimalFormat.format((parsed/100));
-                    current = formatted;
-                    int ver=editText.getSelectionStart();
-                    if(select!=-1){
-                        if(select!=1){
-                            if(editText.getSelectionStart()==1){
-                                select=formatted.length();
+                if(editText.getText().toString().length()>23){
+                    Toast.makeText(Context, "ya a superado la maxima cantidad de dinero que puede digitar", Toast.LENGTH_LONG).show();
+                    editText.setText(maximo);
+                    editText.setSelection(editText.getText().toString().length());
+                }else{
+                    if(!s.toString().equals(current) && s.toString().compareTo("")!=0){
+                        s=s+".00";
+                        editText.removeTextChangedListener(this);
+                        String cleanString = s.toString().replaceAll("["+Context.getResources().getString(R.string.MonedaMonto)+",.]", "").replace(" ","");
+                        double parsed = Double.parseDouble(cleanString.replaceAll("\\s","").trim());
+                        DecimalFormat decimalFormat  = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+                        DecimalFormatSymbols symbols = decimalFormat.getDecimalFormatSymbols();
+                        symbols.setCurrencySymbol(Context.getResources().getString(R.string.MonedaMonto)+" ");
+                        decimalFormat.setDecimalFormatSymbols(symbols);
+                        decimalFormat.setMaximumFractionDigits(0);
+                        String formatted = decimalFormat.format((parsed/100));
+                        current = formatted;
+                        int ver=editText.getSelectionStart();
+                        if(select!=-1){
+                            if(select!=1){
+                                if(editText.getSelectionStart()==1){
+                                    select=formatted.length();
+                                }else{
+                                    select=editText.getSelectionStart();
+                                }
                             }else{
-                                select=editText.getSelectionStart();
+                                select=formatted.length();
                             }
                         }else{
                             select=formatted.length();
                         }
-                    }else{
-                        select=formatted.length();
+                        editText.setText(formatted);
+                        try{
+                            editText.setSelection(select);
+                        }catch (Exception e){
+                            editText.setSelection(formatted.length());
+                        }
+                        editText.addTextChangedListener(this);
+                        //Toast.makeText(Context, ""+editText.getText().toString().length(), Toast.LENGTH_LONG).show();
+                        //editText.getSelectionStart();
+                        editText.setSelection(editText.getText().toString().length());
+                        maximo=editText.getText().toString();
                     }
-                    editText.setText(formatted);
-                    try{
-                        editText.setSelection(select);
-                    }catch (Exception e){
-                        editText.setSelection(formatted.length());
-                    }
-                    editText.addTextChangedListener(this);
                 }
             }
         };
